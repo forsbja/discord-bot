a1ef4fa74e201b9086e55384e1d31f6449ceae7b
Jake Forsberg <forsbja@gmail.com>
2022-11-16 14:28:57 -0700

    Added finished rpg file + started scenarios.json

--- adventure.js
+++ adventure.js
@@ -1,9 +1,75 @@
+const fs = require('fs');
+
 module.exports = {
     
-    startAdventure: async function(message) {
+    startAdventure: async function(message, index) {
+        if(message.author.bot){return;} //Ensure a bot didn't trigger this
+        //Read in JSON File to a variable
+        let rawData = fs.readFileSync('scenarios.json');
+        let scenarios = JSON.parse(rawData);
+
+        //Keep all responses to this in the same file and to the same user
         let currChannel = message.channel;
+        let player = message.author;
         
+        //Set up scenario vars
+        var currScenario = scenarios[0];
+        var choices = currScenario.choices;
+        let transPrompt = "";
+
+        //Send first scenario
+        let scenarioPrompt = formPrompt(currScenario.lore,choices);
+        message.channel.send(scenarioPrompt);
         
+        //Loop for as long as the game runs
+        while(true){
+            //Grab Answer
+            const filter = m => !m.author.bot && m.author == player;
+            const answer = await currChannel.awaitMessages({filter, max: 1, time: 60000})
+            const ans = answer.first();
+
+            //Handle user taking too long and exit the game
+            if(ans == null){
+                await currChannel.send("Oh no! You waited so long to choose you died of old age :) !");
+                console.log("User failed to answer")
+                return;
+            }
+            console.log("Player answered: " + ans.content);
+
+            //Add a transition statement from the last choice to the next scenario
+            transPrompt = await currScenario.choiceTransition[ans.content-1] + " ";
+            console.log(transPrompt);
+           
+            //Set Scenario to next one and update choices
+            currScenario = await scenarios[currScenario.nextScenario[ans.content-1]];
+            choices = await currScenario.choices;
+
+            //Check to see if they chose poorly, exit the game if true
+            if(currScenario.death){
+                currChannel.send(transPrompt + " " + currScenario.lore);
+                return;
+            }
+
+            //Compose message for next scenario and send it
+            scenarioPrompt = transPrompt.concat(formPrompt(currScenario.lore,choices));
+            await message.channel.send(scenarioPrompt);
+            console.log(currScenario);
+        }
     }
     
+}
+
+
+
+
+
+/**
+ * 
+ * @param {*} lore 
+ * @param {*} choices 
+ * @returns 
+ */
+function formPrompt(lore, choices){
+    let scenarioPrompt = lore + "\n1: "+choices[0]+"\n2: "+choices[1]+"\n3: "+choices[2];
+    return scenarioPrompt;
 }
